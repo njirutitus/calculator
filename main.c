b035d0d0cca030a5622e65874e6a8c19852b6108
Titus Njiru <njirutitus@gmail.com>
2022-07-04 12:45:42 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,34 @@
+/*
+project:    Simple calculator that takes two numbers and outputs the sum,
+            difference,product and quotient of the numbers
+Author:     Titus Njiru
+Compiler:   C89
+license:    MIT
+date:       July 2022
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // Variable declaration
+    float num1,num2,sum,difference,product,quotient;
+    printf("\tCalculator!\n");
+    // input
+    printf("Enter two numbers: ");
+    scanf("%f%f",&num1,&num2);
+    // computation
+    // arithmetic operators: + - * / ++ -- %
+    sum = num1 + num2;
+    difference = num1 - num2;
+    product = num1 * num2;
+    quotient = num1 / num2;
+    // output
+    // 20 + 30 = 50
+    printf("%8.2f + %8.2f = %-8.2f\n",num1, num2, sum);
+    printf("%8.2f - %8.2f = %-8.2f\n",num1, num2,difference);
+    printf("%8.2f * %8.2f = %-8.2f\n",num1,num2,product);
+    printf("%8.2f / %8.2f = %-8.2f\n",num1,num2,quotient);
+    return 0;
+}
